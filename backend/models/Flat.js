d10000e8ebc939b9841b544e42e96f2d4015a9ac
Jia Chin <n10215140@qut.edu.au>
2025-08-10 18:26:36 +1000

    update on flat controller, js files, frontend pages for flatfeatures

--- backend/models/Flat.js
+++ backend/models/Flat.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 
 const flatSchema = new mongoose.Schema({
     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    flatNo: { type: String, required: true },
+    address: { type: String, required: true },
     description: { type: String },
-    completed: { type: Boolean, default: false },
-    deadline: { type: Date },
+    vacant: { type: Boolean, default: false },
+    inspectionDate: { type: Date },
 });
 
 module.exports = mongoose.model('Flat', flatSchema);
