a78864db09db6b58f577bec34d4d8b907217f43c
Jia Chin <n10215140@qut.edu.au>
2025-08-13 20:47:13 +1000

    added public address

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5001', // local
+  //baseURL: 'http://localhost:5001', // local
   baseURL: 'http://54.79.173.67', // live
   headers: { 'Content-Type': 'application/json' },
 });
