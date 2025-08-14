0ef4b2a3c9e7e46e92fd4d3f31a46377eafba9ba
Jia Chin <n10215140@qut.edu.au>
2025-08-15 00:54:44 +1000

    update live address

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   //baseURL: 'http://localhost:5001', // local
-  baseURL: 'http://3.27.120.20', // live
+  baseURL: 'http://3.27.120.223', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
