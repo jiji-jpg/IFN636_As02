876cd9ce2c0b1a57998e530fa36689886212cdcf
Jia Chin <n10215140@qut.edu.au>
2025-08-15 16:35:31 +1000

    update live link adding :5001

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   //baseURL: 'http://localhost:5001', // local
-  baseURL: 'http://54.253.134.46', // live
+  baseURL: 'http://54.253.134.46:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
